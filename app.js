17c6b8c19d7fbae0a8c94f655e0236d005b32580
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-19 16:47:23 -0700

    Completed parts 1 and 2

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Let's get this party started!");
+"use strict";
+
+/** Takes an event parameter,
+ * queries Giphy API with users search input,
+ * and appends the resulting gif to the DOM. */
+
+async function search(evt) {
+  evt.preventDefault();
+
+  const searchText = $("#search-text").val();
+  const params = new URLSearchParams({
+    q: searchText,
+    api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  });
+
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?${params}`);
+  const responseObj = await response.json();
+  console.log(responseObj);
+
+  const url = responseObj.data[0].images.downsized.url;
+  $("#gifs").append($("<img>").attr("src", url));
+}
+
+$("#search-button").on("click", search);
